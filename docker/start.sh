7d1e783f216435bc1e72a944da937fe6cc780da8
Leon Seng <w.leon.seng@gmail.com>
2024-12-05 15:31:11 +1100

    Addded --replace flag to GH action runner config script

--- docker/start.sh
+++ docker/start.sh
@@ -9,6 +9,7 @@ REG_TOKEN=$(curl -X POST -H "Authorization: token ${GITHUB_PERSONAL_ACCESS_TOKEN
   --url $GITHUB_REPO_URL \
   --token $REG_TOKEN \
   --unattended \
-  --disableupdate
+  --disableupdate \
+  --replace
 
 ./run.sh
