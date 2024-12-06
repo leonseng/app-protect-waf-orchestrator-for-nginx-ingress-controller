a23662bca78660830d1f44ece49fd581871c42cd
Leon Seng <w.leon.seng@gmail.com>
2024-12-06 11:56:40 +1100

    Added comments to GH Action runner start.sh

--- docker/start.sh
+++ docker/start.sh
@@ -2,9 +2,11 @@
 
 set -e
 
+# dynamically add a runner on GitHub and retrieve the registration token
 GITHUB_REPO=$(echo $GITHUB_REPO_URL | awk -F'github.com/' '{print $2}')
 REG_TOKEN=$(curl -X POST -H "Authorization: token ${GITHUB_PERSONAL_ACCESS_TOKEN}" -H "Accept: application/vnd.github+json" https://api.github.com/repos/${GITHUB_REPO}/actions/runners/registration-token | jq .token --raw-output)
 
+# configure and start agent using registration token obtained from above.
 ./config.sh \
   --url $GITHUB_REPO_URL \
   --token $REG_TOKEN \
